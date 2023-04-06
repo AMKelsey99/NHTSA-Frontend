a132169caed738f640d974a67b14a0b13ff07d56
AMKelsey99 <103036970+AMKelsey99@users.noreply.github.com>
2023-04-06 06:32:50 -0400

    Delete CS410Frontend directory

--- CS410Frontend/cs410/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class Cs410Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cs410'
-    verbose_name = 'placeholder'
--- CS410Frontend/cs410/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from cs410 import views
-
-urlpatterns = [
-    path("main/", views.main, name="main"),
-    path('search/',views.search, name='search'),
-]
